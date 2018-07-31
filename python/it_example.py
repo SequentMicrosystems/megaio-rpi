b444996754692878c5ede5e6a81a71d96ae41b3f
Alexandru Burcea <olcitu@gmail.com>
2018-07-31 23:35:49 +0300

    minor

--- python/it_example.py
+++ python/it_example.py
@@ -17,15 +17,15 @@ m.add_opto_event(0, 1, m.RISING, opto_callback)
 m.add_opto_event(0, 2, m.RISING, opto_callback)
 
 try:
-  print "This example show you how to use external interrupt on megaio board"
+  print ("This example show you how to use external interrupt on megaio board")
   
-  print "Connect pin 2 and 4 for optocuplers power(+5V and 5VEXT)"
-  print "Momentary connect opto channel 1 and 2 (pin 3 and 5) to GND"
-  print "for generating the interrupts"
-  print "Waith for opto events.." 
+  print ("Connect pin 2 and 4 for optocuplers power(+5V and 5VEXT)")
+  print ("Momentary connect opto channel 1 and 2 (pin 3 and 5) to GND")
+  print ("for generating the interrupts")
+  print ("Waith for opto events..")
   while 1:
     time.sleep(.02)
-  print "end wait"
+  print ("end wait")
   
 except KeyboardInterrupt:  
-  print "Interrupted"
+  print ("Interrupted")
